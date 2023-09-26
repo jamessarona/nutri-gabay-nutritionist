555acba80ca2314f5287420bfac22dc58ca3ae57
James Angelo Sarona <jacsarona.official@gmail.com>
2023-09-26 23:33:30 +0800

    added landing page with navigation bar

--- lib/views/ui/main_page.dart
+++ lib/views/ui/main_page.dart
@@ -1,5 +1,12 @@
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nutri_gabay_nutritionist/services/baseauth.dart';
+import 'package:nutri_gabay_nutritionist/views/shared/app_style.dart';
+import 'package:nutri_gabay_nutritionist/views/ui/appointment_page.dart';
+import 'package:nutri_gabay_nutritionist/views/ui/dashboard_page.dart';
+import 'package:nutri_gabay_nutritionist/views/ui/patient_list_page.dart';
+import 'package:universal_html/html.dart' as html;
 
 class MainPage extends StatefulWidget {
   final BaseAuth auth;
@@ -11,8 +18,111 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  PageController pageController = PageController();
+  SideMenuController sideMenu = SideMenuController();
+
+  bool isHideNavBar = false;
+
+  @override
+  void initState() {
+    sideMenu.addListener((index) {
+      pageController.jumpToPage(index);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SideMenu(
+            controller: sideMenu,
+            showToggle: false,
+            style: SideMenuStyle(
+              backgroundColor: Colors.white,
+              displayMode: SideMenuDisplayMode.auto,
+              hoverColor: customColor,
+              selectedHoverColor: customColor,
+              selectedColor: customColor,
+              selectedTitleTextStyle: const TextStyle(color: Colors.white),
+              selectedIconColor: Colors.white,
+            ),
+            title: Column(
+              children: [
+                const SizedBox(height: 10),
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxHeight: 100,
+                    maxWidth: 150,
+                  ),
+                  child: Image.asset(
+                    'assets/images/logo-name.png',
+                  ),
+                ),
+                const Divider(
+                  indent: 8.0,
+                  endIndent: 8.0,
+                ),
+              ],
+            ),
+            alwaysShowFooter: true,
+            footer: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SideMenuItem(
+                  title: 'Settings',
+                  onTap: (index, _) {},
+                  icon: const Icon(Icons.settings),
+                ),
+                SideMenuItem(
+                  title: 'Exit',
+                  icon: const Icon(Icons.exit_to_app),
+                  onTap: (index, _) {
+                    FireBaseAuth()
+                        .signOut()
+                        .then((value) async => html.window.location.reload());
+                  },
+                ),
+              ],
+            ),
+            items: [
+              SideMenuItem(
+                title: 'Dashboard',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(FontAwesomeIcons.userDoctor),
+              ),
+              SideMenuItem(
+                title: 'Patients',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(FontAwesomeIcons.userInjured),
+              ),
+              SideMenuItem(
+                title: 'Appointments',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(FontAwesomeIcons.clipboardList),
+              ),
+            ],
+          ),
+          Expanded(
+            child: PageView(
+              controller: pageController,
+              children: const [
+                DashboardPage(),
+                PatientListPage(),
+                AppointmentPage(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/ui/patient_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PatientListPage extends StatefulWidget {
+  const PatientListPage({super.key});
+
+  @override
+  State<PatientListPage> createState() => _PatientListPageState();
+}
+
+class _PatientListPageState extends State<PatientListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
