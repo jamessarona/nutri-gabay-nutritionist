863301ace622237ea55c9b0d2686bf0c206299fc
James Angelo Sarona <jacsarona.official@gmail.com>
2023-10-09 22:47:23 +0800

    added initial background layout for appointment

--- lib/views/ui/appointment_page.dart
+++ lib/views/ui/appointment_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:nutri_gabay_nutritionist/views/shared/app_style.dart';
 
 class AppointmentPage extends StatefulWidget {
   const AppointmentPage({super.key});
@@ -8,14 +10,87 @@ class AppointmentPage extends StatefulWidget {
 }
 
 class _AppointmentPageState extends State<AppointmentPage> {
+  late Size screenSize;
+  final Stream<QuerySnapshot> _pendingAppointment = FirebaseFirestore.instance
+      .collection('appointment')
+      .where("status", isNotEqualTo: "Pending")
+      .snapshots();
   @override
   Widget build(BuildContext context) {
+    screenSize = MediaQuery.of(context).size;
     return Container(
       height: double.infinity,
       width: double.infinity,
       color: Colors.grey.shade100,
-      child: ListView(
-        children: [],
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            height: 50,
+            width: double.infinity,
+            color: customColor[70],
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Appointment Request',
+              style: appstyle(
+                25,
+                Colors.black,
+                FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(top: 25, left: 15, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    width: screenSize.width * 0.4,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: _pendingAppointment,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return const Text('Something went wrong');
+                        }
+
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (!snapshot.hasData) {
+                          return const Text('No Records');
+                        }
+                        // Bug Parent Height
+                        return SizedBox(
+                          height: 1000,
+                          child: ListView.builder(
+                            itemCount: snapshot.data!.docs.length,
+                            scrollDirection: Axis.vertical,
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot ds = snapshot.data!.docs[index];
+                              return Text('data');
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: screenSize.width * 0.4,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
